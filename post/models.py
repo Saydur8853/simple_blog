4c23857c745bbea497acaf868bc37dd32873099f
saydur <saydur@co.design>
2023-05-09 12:28:30 +0600

    done so far

--- post/models.py
+++ post/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from datetime import datetime
+from django.urls import reverse
+
+# from .views import post
 
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.CharField(max_length=1000000)
+    created_at = models.DateTimeField(default=datetime.now, blank=True)
+
+    def get_detail_url(self):
+        return reverse("post", args=[str(self.pk)])
