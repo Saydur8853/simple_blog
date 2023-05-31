242d106a8c3a8157fd48f088d1ced1fc8b833cd7
saydur <saydur@co.design>
2023-05-31 15:10:11 +0600

    refactor: :boom: initial boilerplate

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for blog project.
+URL configuration for app project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -12,13 +12,11 @@ Class-based views
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+    2. Add a URL to urlpatterns:  path('app/', include('app.urls'))
 """
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("post.urls")),
-    # path("", include("weather.urls")),
 ]
--- post/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from post import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("weather/", views.weather, name="weather"),
-    path("post/<str:pk>", views.post, name="post"),
-]
