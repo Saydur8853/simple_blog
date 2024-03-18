6cddb45b0281514a37031d841662b80010b8bcde
saydur8853 <saydur774@gmail.com>
2024-03-18 16:02:25 +0600

    somthing funny added

--- app/urls.py
+++ app/urls.py
@@ -18,8 +18,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-
+from testapp import views as testrunview
 
 urlpatterns = [
+    path('',testrunview.home, name='home'),
     path("admin/", admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
